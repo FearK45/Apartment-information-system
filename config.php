9f58eb6cba3ca00f2db7dd20ba4e9721a5ac6fd8
FearK45 <kids67550888@gmail.com>
2026-06-05 19:17:20 +0530

    Add config file

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'apartment_system';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if($conn->connect_error) {
+    die('DB Connection Error: ' . $conn->connect_error);
+}
+
+$conn->set_charset('utf8');
+?>
